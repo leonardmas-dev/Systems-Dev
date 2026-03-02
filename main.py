060f2d4625e9c0873d1bec4fae86654474badef1
Thierno543 <thierno2.batiga@live.uwe.ac.uk>
2026-03-02 16:06:24 +0000

    send

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import tkinter as tk
 
 root = tk.Tk()
 # Widgets are added here
-label = tk.Label(root, text="Hi")
+label = tk.Label(root, text="Hello")
 label.pack()
 root.mainloop()
-<<<<<<< HEAD
-=======
-
-print("Hi")
->>>>>>> 0eccef323966dfa381cf5b4cbffd9317a4ff7ddb
