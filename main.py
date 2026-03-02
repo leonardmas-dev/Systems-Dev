a616decff4237f890fbaab7c79fc8316f9282261
Yaseen <yaseen2.sassi@live.uwe.ac.uk>
2026-03-02 16:04:45 +0000

    update

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ root = tk.Tk()
 label = tk.Label(root, text="Hi")
 label.pack()
 root.mainloop()
-<<<<<<< HEAD
-=======
 
-print("Hi")
->>>>>>> 0eccef323966dfa381cf5b4cbffd9317a4ff7ddb
+print("HEllo")
